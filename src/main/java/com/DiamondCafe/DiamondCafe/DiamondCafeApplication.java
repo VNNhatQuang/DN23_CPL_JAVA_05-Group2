9a317baf9e88b681badba15c69d38f0a8c2ba278
VNNQuang <quangvip2611@gmail.com>
2023-05-02 14:24:13 +0700

    create project

---
+++ src/main/java/com/DiamondCafe/DiamondCafe/DiamondCafeApplication.java
@@ -0,0 +1,13 @@
+package com.DiamondCafe.DiamondCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondCafeApplication.class, args);
+	}
+
+}
