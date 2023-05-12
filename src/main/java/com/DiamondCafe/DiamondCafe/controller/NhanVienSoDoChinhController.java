9983af535f360df6aacf57c7020db1909fe79a0d
VNNQuang <quangvip2611@gmail.com>
2023-05-12 13:29:39 +0700

    update

--- src/main/java/com/DiamondCafe/DiamondCafe/controller/NhanVienSoDoChinhController.java
+++ src/main/java/com/DiamondCafe/DiamondCafe/controller/NhanVienSoDoChinhController.java
@@ -1,11 +1,11 @@
 package com.DiamondCafe.DiamondCafe.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -182,4 +182,5 @@ public class NhanVienSoDoChinhController {
 		sdcService.EmptyTable(SoBan);
 		return "redirect:/home";
 	}
+
 }
